8a838223221804445c3e2c74b71556b6acbb682a
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-10-18 00:14:43 +0000

    Update demo to include a funvas animation (#7)
    * Refactor example
    
    * Add funvas animation
    
    * Add slider for funvas
    
    * Update layout

--- performance/example/lib/main.dart
+++ performance/example/lib/main.dart
@@ -1,43 +1,100 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:funvas/funvas.dart';
+import 'package:performance_example/widgets/funvas.dart';
+import 'package:performance_example/widgets/link.dart';
 import 'package:url_strategy/url_strategy.dart';
 
 void main() {
   setPathUrlStrategy();
-  runApp(const MyApp());
+  runApp(const ExampleApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ExampleApp extends StatelessWidget {
+  const ExampleApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'performance overlay demo',
       theme: ThemeData(
         brightness: Brightness.dark,
         primarySwatch: Colors.amber,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const _HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+class _HomePage extends StatefulWidget {
+  const _HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<_HomePage> {
+  late final _funvas = ExampleFunvas();
+
+  var _funvasSize = 5 / 8;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      body: Row(
+        children: [
+          const Spacer(),
+          Expanded(
+            flex: 2,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        FractionallySizedBox(
+                          widthFactor: _funvasSize,
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: FunvasContainer(
+                              funvas: _funvas,
+                            ),
+                          ),
+                        ),
+                        const Link(
+                          url: 'https://funvas.creativemaybeno.dev/#/32',
+                          body: Text('funvas animation'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 16,
+                  ),
+                  child: FractionallySizedBox(
+                    widthFactor: 3 / 4,
+                    child: Slider(
+                      min: 4 / 8,
+                      max: 6 / 8,
+                      value: _funvasSize,
+                      onChanged: (value) {
+                        setState(() {
+                          _funvasSize = value;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Spacer(),
+        ],
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }

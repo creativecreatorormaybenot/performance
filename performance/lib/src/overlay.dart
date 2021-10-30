bdf5a6d3e4d14e92e988da734a3f8979aa7b2242
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-10-30 17:47:20 +0000

    Skip bad sample

--- performance/lib/src/overlay.dart
+++ performance/lib/src/overlay.dart
@@ -215,6 +215,7 @@ class _CustomPerformanceOverlay extends StatefulWidget {
 
 class _CustomPerformanceOverlayState extends State<_CustomPerformanceOverlay> {
   var _samples = <FrameTiming>[];
+  var _skippedFirstSample = false;
 
   @override
   void initState() {
@@ -230,6 +231,14 @@ class _CustomPerformanceOverlayState extends State<_CustomPerformanceOverlay> {
 
   void _timingsCallback(List<FrameTiming> frameTimings) {
     if (!mounted) return;
+
+    if (!_skippedFirstSample) {
+      // Throw away the first sample since it seems to always be some cumulative
+      // value that does not at all represent actual performance.
+      frameTimings = frameTimings.sublist(1);
+      _skippedFirstSample = true;
+    }
+
     final combinedSamples = [
       ..._samples,
       ...frameTimings,
